52570bfc6a51bca0e0f5502140ea8377c4e51834
Reno Sifana Paksi <renosifana.paksi@gmail.com>
2025-02-25 15:08:02 +0700

    merge check-link-sh from https://github.com/aerphanas/Kumpulan-ISO-Distro

--- check-link.sh
+++ check-link.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-link=$(grep https README.md | cut -d ']' -f 2 | tr -d "()")
+set -eo pipefail
 
-for i in $link
-do
-	wget --spider $i
-done
+link=$(grep -oP '(?<=\]\().*?(?=\))' README.md)
+
+while IFS= read -r i; do
+    status_code=$(curl --head --silent --location --output /dev/null --write-out "%{http_code}" "$i")
+    if [ "$status_code" -ne 200 ]; then
+        echo "Error accessing $i Status code: $status_code"
+        exit 1
+    fi
+done <<< "$link"
+
+echo "All links are accessible"
+exit 0
